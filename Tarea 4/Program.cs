f5aaec7b2fc8c7716d85ec3c170ed3a7eb6edb2c
Daniel G <49257382+HeVyBlack@users.noreply.github.com>
2022-04-25 17:32:39 -0500

    FInal

--- Tarea 4/Program.cs
+++ Tarea 4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Text;
 using System.Xml;
 
@@ -7,78 +7,11 @@ namespace Tarea4
 {
     internal class Program
     {
-        
+
         static void Main(string[] args)
         {
             int[] lista = new int[10];
             string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            Boolean xmlFind = false;
-            Boolean txtFind = false;
-            if (File.Exists(path + @"\archivo.txt"))
-            {
-                Console.WriteLine("Archivo .txt encontrado");
-                txtFind = true;
-            }else
-            {
-                Console.WriteLine("Archivo .txt NO encontrado");
-            }
-            if (File.Exists(path + @"\archivo.xml"))
-            {
-                Console.WriteLine("Archivo .xml encontrado");
-                xmlFind = true;
-            }else
-            {
-                Console.WriteLine("Archivo .xml NO encontrado");
-            }
-            if (xmlFind == true && txtFind == true)
-            {
-                Console.WriteLine("Los dos archivos fueron encontrados, puede seguir.");
-            }else if (xmlFind == false && txtFind == false)
-            {
-                Console.WriteLine("Los dos archivos NO fueron encontrados, puede seguir, pero si desea puede crearlos.");
-                Console.WriteLine("Da Enter para crearlo o cualquier tecla para continuar sin el archivo. Los datos se borraran al momemento de cerrar el programa");
-                var igual = Console.ReadKey();
-                if (igual.Key == ConsoleKey.Enter)
-                {
-                    CrearArchivoTxt();
-                    CrearArchivoXml();
-                }else
-                {
-                    Console.Clear();
-                    Console.WriteLine("Continuaras sin crear ningún archivo. Los datos se perderan al finalizar el programa");
-                }
-
-            }else if (xmlFind == true || txtFind == false)
-            {
-                Console.WriteLine("Archivo .txt No encontrado pero .xml si fue encontrado, puede seguir, pero si desea puede crearlo.");
-                Console.WriteLine("Da Enter para crearlo o cualquier tecla para continuar sin el archivo.\nLos datos se borraran al momemento de cerrar el programa");
-                var igual = Console.ReadKey();
-                if (igual.Key == ConsoleKey.Enter)
-                {
-                    CrearArchivoTxt();
-                }
-                else
-                {
-                    Console.Clear();
-                    Console.WriteLine("Continuaras sin crear ningún archivo. Los datos se perderan al finalizar el programa");
-                }
-
-            }else if (xmlFind == false || txtFind == true)
-            {
-                Console.WriteLine("Archivo .xml No encontrado pero .txt si fue encontrado, puede seguir, pero si desea puede crearlo.");
-                Console.WriteLine("Da Enter para crearlo o cualquier tecla para continuar sin el archivo.\nLos datos se borraran al momemento de cerrar el programa");
-                var igual = Console.ReadKey();
-                if (igual.Key == ConsoleKey.Enter)
-                {
-                    CrearArchivoXml();
-                }
-                else
-                {
-                    Console.Clear();
-                    Console.WriteLine("Continuaras sin crear ningún archivo. Los datos se perderan al finalizar el programa");
-                }
-
-            }
 
             Console.WriteLine("Para este programa, se te pedirá una serie de 10 números, luego se te pedira el metodo para ordenenar esos números. ");
             string x;
@@ -96,25 +29,149 @@ namespace Tarea4
                     Console.WriteLine("Número incorrecto, vuelve a intentar:");
                 }
                 lista[index] = Convert.ToInt32(x);
-                
+
             }
+            pre:
+            int pre;
             Console.Clear();
-            Console.WriteLine("Ahora selecciona el metodo de organización: \n 1 = Burbuja");
-            var y = Console.ReadLine();
-            if (y == "1")
+            Console.WriteLine("Ahora selecciona el metodo de organización: \n 1 = Inserción | 2 = Selección | 3 = Shell | 4 = Burbuja");
+            while (true)
+            {
+                var h = Convert.ToString(Console.ReadLine());
+
+                if (int.TryParse(h, out int val) == true)
+                {
+                    pre = Convert.ToInt32(h);
+                    break;
+                }
+                Console.WriteLine("Número incorrecto, vuelve a intentar:");
+            }
+            if (pre == 1)
+            {
+                Console.Clear();
+                metodoInsercion(lista);
+                Console.WriteLine("Inserción");
+
+            }else if (pre == 2)
+            {
+                Console.Clear();
+                metodoSeleccion(lista);
+                Console.WriteLine("Selección");
+
+            }else if (pre == 3)
+            {
+                Console.Clear();
+                metodoShell(lista);
+                Console.WriteLine("Shell");
+
+            }else if (pre == 4)
             {
                 Console.Clear();
                 metodoBurbuja(lista);
+                Console.WriteLine("Burbuja");
             }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Número equivocado");
+                goto pre;
+            }
+
+
         }
 
+        public static void metodoInsercion(int[] lista)
+        {
+            int ordenamiento = 1;
+            for (int i = 0; i < lista.Length; i++)
+            {
+                var clave = lista[i];
+                var j = i - 1;
+                while (j >= 0 && lista[j] > clave)
+                {
+                    lista[j + 1] = lista[j];
+                    j--;
+                }
+                lista[j + 1] = clave;
+                Console.WriteLine($"El ordenamiento No {ordenamiento} : ");
+                listaShow(lista);
+                ordenamiento++;
+            }
+            PreguntaArchivo(lista);
+
+        }
+
+        public static void metodoSeleccion(int[] lista)
+        {
+            int ordenamiento = 1;
+            int menor = 0;
+            int aux = 0;
+            int aux2 = 0;
+            for (int i = 0; i < lista.Length - 1; i++)
+            {
+                menor = lista[i];
+                aux = i;
+                for (int j = i + 1; j < lista.Length; j++)
+                {
+                    if (lista[j] < menor)
+                    {
+                        menor = lista[j];
+                        aux = j;
+                    }
+                }
+                if (aux != i)
+                {
+                    aux2 = lista[i];
+                    lista[i] = lista[aux];
+                    lista[aux] = aux2;
+                }
+                Console.WriteLine($"El ordenamiento No {ordenamiento} : ");
+                listaShow(lista);
+                ordenamiento++;
+            }
+            PreguntaArchivo(lista);
+        }
+        public static void metodoShell(int[] lista)
+        {
+            int mitad = 0;
+            bool bucle = false;
+            int aux = 0;
+            int x = 0;
+            int ordenamiento = 1;
+            mitad = lista.Length / 2;
+            while (mitad > 0)
+            {
+                bucle = true;
+                while (bucle)
+                {
+                    bucle = false;
+                    x = 1;
+                    while (x <= (lista.Length - mitad))
+                    {
+                        if (lista[x - 1] > lista[(x - 1) + mitad])
+                        {
+                            aux = lista[(x - 1) + mitad];
+                            lista[(x - 1) + mitad] = lista[x - 1];
+                            lista[(x - 1)] = aux;
+                            bucle = true;
+                            Console.WriteLine($"El ordenamiento No {ordenamiento} : ");
+                            listaShow(lista);
+                            ordenamiento++;
+                        }
+                        x++;
+                    }
+                }
+                mitad = mitad / 2;
+            }
+            PreguntaArchivo(lista);
+        }
         public static void metodoBurbuja(int[] lista)
         {
             int ordenamiento = 1;
 
             for (int i = 0; i < lista.Length; i++)
             {
-                for (int j = 0; j < lista.Length -i -1; j++)
+                for (int j = 0; j < lista.Length - i - 1; j++)
                 {
 
                     if (lista[j] > lista[j + 1])
@@ -128,35 +185,107 @@ namespace Tarea4
                 listaShow(lista);
                 ordenamiento++;
             }
+            PreguntaArchivo(lista);
         }
 
+        public static void PreguntaArchivo(int[]lista)
+        {
+            string listaString = "";
+            for (int i = 0; i < lista.Length; i++)
+            {
+                if(i == lista.Length-1)
+                {
+                    listaString += lista[i];
+                    break;
+                }
+                listaString += lista[i] + ", ";
+            }
+
+            Console.WriteLine("Escoje entre: Guardar el arcivo en formato XML, Txt o si quieres salir sin hacer nada.\nApreta: 1. XML | 2.Txt | Cualquier tecla. Salir");
+
+            try
+            {
+                var pr1 = Convert.ToInt32(Console.ReadLine());
+                if (pr1 == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Como te gustaría llamar al arcivo? \n Escribe el nombre");
+                    var x = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Seguro te gustaría llamarlo así? \n si lo quieres cambiar apreta cualquier tecla, si no, aprieta Enter");
+                    var y = Console.ReadKey();
+                    if (y.Key == ConsoleKey.Enter)
+                    {
+                        CrearArchivoXml(listaString, x);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Escribe el nuevo nombre");
+                        x = Convert.ToString(Console.ReadLine());
+                        CrearArchivoXml(listaString, x);
+                    }
+                }
+                else if (pr1 == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Como te gustaría llamar al arcivo? \n Escribe el nombre");
+                    var x = Convert.ToString(Console.ReadLine());
+                    Console.WriteLine("Seguro te gustaría llamarlo así? \n si lo quieres cambiar aprieta cualquier tecla, si no, apreta Enter");
+                    var y = Console.ReadKey();
+                    if (y.Key == ConsoleKey.Enter)
+                    {
+                        CrearArchivoTxt(listaString, x);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Escribe el nuevo nombre");
+                        x = Convert.ToString(Console.ReadLine());
+                        CrearArchivoTxt(listaString, x);
+                    }
+                }
+                else
+                {
+                    Environment.Exit(0);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Environment.Exit(0);
+            }
+
+            
+
+                
+        }
         public static string listaShow(int[] lista)
         {
-            foreach(var number in lista)
+            foreach (var number in lista)
             {
-                Console.Write(number + ",");
-            }Console.WriteLine("\n");
+                Console.Write(number + ", ");
+            }
+            Console.WriteLine("\n");
             return "";
         }
-        public static void CrearArchivoTxt()
+        public static void CrearArchivoTxt(string listaString, string x)
         {
             string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            using (FileStream fs = File.Create(path + @"\archivo.txt"))
+            using (FileStream fs = File.Create(path + $@"\{x}.txt"))
             {
-                byte[] info = new UTF8Encoding(true).GetBytes("This is some text in the file.");
-                // Add some information to the file.
+                byte[] info = new UTF8Encoding(true).GetBytes(listaString);
                 fs.Write(info, 0, info.Length);
             }
             Console.WriteLine("Archivo .txt listo");
         }
-        public static void CrearArchivoXml()
+        public static void CrearArchivoXml(string listaString, string x)
         {
             string path =
             Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-            XmlWriter xmlWriter = XmlWriter.Create(path + @"\archivo.xml");
+            XmlWriter xmlWriter = XmlWriter.Create(path + $@"\{x}.xml");
 
             xmlWriter.WriteStartDocument();
             xmlWriter.WriteStartElement("ListaNums");
+            xmlWriter.WriteStartElement("Lista");
+            xmlWriter.WriteString(Environment.NewLine +  listaString + Environment.NewLine);
             xmlWriter.WriteEndElement();
             xmlWriter.WriteEndDocument();
             xmlWriter.Close();
